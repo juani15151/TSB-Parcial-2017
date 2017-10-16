a853227ff5431eb3eb984ee8fa237a7ce1e49053
juani15151 <wfalconwf@gmail.com>
2017-10-16 20:11:08 -0300

    Nuevos test más completos.

--- Parcial Unico/ParcialUnico2017/src/test/java/clases/TSBDeQueueTest.java
+++ Parcial Unico/ParcialUnico2017/src/test/java/clases/TSBDeQueueTest.java
@@ -1,5 +1,6 @@
 package clases;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import org.junit.After;
@@ -12,79 +13,117 @@ import static org.junit.Assert.*;
  *
  * @author Ing. Valerio Frittelli.
  */
-public class TSBDeQueueTest 
-{
+public class TSBDeQueueTest {
+
     private TSBDeQueue<Integer> instance;
+    private TSBDeQueue<Integer> instance2;
     private TSBDeQueue<Integer> empty;
 
-    public TSBDeQueueTest() 
-    {
+    public TSBDeQueueTest() {
     }
-    
+
     @BeforeClass
-    public static void setUpClass() 
-    {
+    public static void setUpClass() {
     }
-    
+
     @AfterClass
-    public static void tearDownClass() 
-    {
+    public static void tearDownClass() {
     }
-    
+
     @Before
-    public void setUp() 
-    {
+    public void setUp() {
         System.out.println("* UtilsJUnit4Test: @Before method");
         int data[] = {1, 5, 7, 6, 10, 7, 8, 11};
-        
+
         empty = new TSBDeQueue<>();
-        
+
         instance = new TSBDeQueue<>();
-        for(int value : data)
-        {
+        instance2 = new TSBDeQueue<>();
+        for (int value : data) {
             instance.add(value);
+            instance2.add(value);
         }
+
     }
-    
+
     @After
-    public void tearDown() 
-    {
+    public void tearDown() {
     }
-    
+
+    /**
+     * Test de los constructores de la clase TSBDeQueue.
+     */
+    @org.junit.Test
+    public void testConstructors() {
+        // No debe lanzar excepciones.
+        TSBDeQueue<Integer> cola;
+        cola = new TSBDeQueue();
+        cola = new TSBDeQueue(-1);
+        ArrayList<Integer> data = new ArrayList<>();
+        data.add(10);
+        data.add(20);
+        cola = new TSBDeQueue(data);
+        assertEquals(2, cola.size());
+    }
+
     /**
      * Test of add method, of class TSBDeQueue.
      */
-    @org.junit.Test (expected = NullPointerException.class)
-    public void testAdd() 
-    {
+    @org.junit.Test(expected = NullPointerException.class)
+    public void testAdd() {
         System.out.println("add");
+        int size = instance.size();
         instance.add(101);
-        assertEquals((Integer)101, instance.getLast());
-        instance.add(null);    
+        assertEquals((Integer) 101, instance.getLast());
+        assertEquals(size + 1, instance.size());
+        instance.add(null);
+    }
+
+    /**
+     * Prueba que el arreglo crece cuando es necesario.
+     */
+    @org.junit.Test
+    public void testAdaptative() {
+        // No debe lanzar excepciones.
+        TSBDeQueue<Integer> cola = new TSBDeQueue(1);
+        for (int i = 0; i < 100; i++) {
+            cola.add(1);
+        }
     }
 
     /**
      * Test of iterator method, of class TSBDeQueue.
      */
-    @org.junit.Test
-    public void testIterator() 
-    {
+    @org.junit.Test (expected = NoSuchElementException.class)
+    public void testIterator() {
         System.out.println("iterator");
         int ta = instance.size();
         Iterator r1 = instance.iterator();
-        assertNotEquals(null, r1);        
+        assertNotEquals(null, r1);
         assertTrue(r1.hasNext());
         assertNotEquals(null, r1.next());
         r1.remove();
         assertEquals(ta - 1, instance.size());
+        // Next luego de un remove no se debe saltear elementos.
+        assertEquals((Integer) 5, r1.next());
+        // Opcional. 2 invocaciones seguida de remove deben fallar.
+        try {
+            r1.remove();
+            r1.remove(); // Debe lanzar la excepcion aqui.
+            fail(); 
+        } catch (IllegalStateException e) {
+            // Ok
+        }
+        // Si se invoca a next cuando hasNext da falso debe fallar.
+        Iterator i2 = empty.iterator();
+        i2.next();
     }
 
     /**
      * Test of size method, of class TSBDeQueue.
      */
     @org.junit.Test
-    public void testSize() 
-    {
+    public void testSize() {
         System.out.println("size");
         assertEquals(8, instance.size());
         assertEquals(0, empty.size());
@@ -93,60 +132,55 @@ public class TSBDeQueueTest
     /**
      * Test of addFirst method, of class TSBDeQueue.
      */
-    @org.junit.Test (expected = NullPointerException.class)
-    public void testAddFirst() 
-    {
+    @org.junit.Test(expected = NullPointerException.class)
+    public void testAddFirst() {
         System.out.println("addFirst");
         instance.addFirst(99);
-        assertEquals((Integer)99, instance.getFirst());
+        assertEquals((Integer) 99, instance.getFirst());
         instance.addFirst(null);
     }
 
     /**
      * Test of addLast method, of class TSBDeQueue.
      */
-    @org.junit.Test (expected = NullPointerException.class)
-    public void testAddLast() 
-    {
+    @org.junit.Test(expected = NullPointerException.class)
+    public void testAddLast() {
         System.out.println("addLast");
         instance.addLast(101);
-        assertEquals((Integer)101, instance.getLast());
-        instance.addLast(null);    
+        assertEquals((Integer) 101, instance.getLast());
+        instance.addLast(null);
     }
 
     /**
      * Test of offerFirst method, of class TSBDeQueue.
      */
-    @org.junit.Test (expected = NullPointerException.class)
-    public void testOfferFirst() 
-    {
+    @org.junit.Test(expected = NullPointerException.class)
+    public void testOfferFirst() {
         System.out.println("offerFirst");
         assertTrue(instance.offerFirst(99));
-        assertEquals((Integer)99, instance.getFirst());
+        assertEquals((Integer) 99, instance.getFirst());
         instance.offerFirst(null);
     }
 
     /**
      * Test of offerLast method, of class TSBDeQueue.
      */
-    @org.junit.Test (expected = NullPointerException.class)
-    public void testOfferLast() 
-    {
+    @org.junit.Test(expected = NullPointerException.class)
+    public void testOfferLast() {
         System.out.println("offerLast");
         assertTrue(instance.offerLast(101));
-        assertEquals((Integer)101, instance.getLast());
+        assertEquals((Integer) 101, instance.getLast());
         instance.offerLast(null);
     }
 
     /**
      * Test of removeFirst method, of class TSBDeQueue.
      */
-    @org.junit.Test (expected = NoSuchElementException.class)
-    public void testRemoveFirst() 
-    {
+    @org.junit.Test(expected = NoSuchElementException.class)
+    public void testRemoveFirst() {
         System.out.println("removeFirst");
         int ta = instance.size();
-        assertEquals((Integer)1, instance.removeFirst());
+        assertEquals((Integer) 1, instance.removeFirst());
         assertEquals(ta - 1, instance.size());
         int r1 = empty.removeFirst();
     }
@@ -154,24 +188,23 @@ public class TSBDeQueueTest
     /**
      * Test of removeLast method, of class TSBDeQueue.
      */
-    @org.junit.Test (expected = NoSuchElementException.class)
-    public void testRemoveLast() 
-    {
+    @org.junit.Test(expected = NoSuchElementException.class)
+    public void testRemoveLast() {
         System.out.println("removeLast");
         int ta = instance.size();
-        assertEquals((Integer)11, instance.removeLast());
+        assertEquals((Integer) 11, instance.removeLast());
         assertEquals(ta - 1, instance.size());
-        int r1 = empty.removeLast();    }
+        int r1 = empty.removeLast();
+    }
 
     /**
      * Test of pollFirst method, of class TSBDeQueue.
      */
     @org.junit.Test
-    public void testPollFirst() 
-    {
+    public void testPollFirst() {
         System.out.println("pollFirst");
         int ta = instance.size();
-        assertEquals((Integer)1, instance.pollFirst());
+        assertEquals((Integer) 1, instance.pollFirst());
         assertEquals(ta - 1, instance.size());
         assertNull(empty.pollFirst());
     }
@@ -180,11 +213,10 @@ public class TSBDeQueueTest
      * Test of pollLast method, of class TSBDeQueue.
      */
     @org.junit.Test
-    public void testPollLast() 
-    {
+    public void testPollLast() {
         System.out.println("pollLast");
         int ta = instance.size();
-        assertEquals((Integer)11, instance.pollLast());
+        assertEquals((Integer) 11, instance.pollLast());
         assertEquals(ta - 1, instance.size());
         assertNull(empty.pollLast());
     }
@@ -192,32 +224,30 @@ public class TSBDeQueueTest
     /**
      * Test of getFirst method, of class TSBDeQueue.
      */
-    @org.junit.Test (expected = NoSuchElementException.class)
-    public void testGetFirst() 
-    {
+    @org.junit.Test(expected = NoSuchElementException.class)
+    public void testGetFirst() {
         System.out.println("getFirst");
-        assertEquals((Integer)1, instance.getFirst());
+        assertEquals((Integer) 1, instance.getFirst());
         int r1 = empty.getFirst();
     }
 
     /**
      * Test of getLast method, of class TSBDeQueue.
      */
-    @org.junit.Test (expected = NoSuchElementException.class)
-    public void testGetLast() 
-    {
+    @org.junit.Test(expected = NoSuchElementException.class)
+    public void testGetLast() {
         System.out.println("getLast");
-        assertEquals((Integer)11, instance.getLast());
-        int r1 = empty.getLast();    }
+        assertEquals((Integer) 11, instance.getLast());
+        int r1 = empty.getLast();
+    }
 
     /**
      * Test of peekFirst method, of class TSBDeQueue.
      */
     @org.junit.Test
-    public void testPeekFirst() 
-    {
+    public void testPeekFirst() {
         System.out.println("peekFirst");
-        assertEquals((Integer)1, instance.peekFirst());
+        assertEquals((Integer) 1, instance.peekFirst());
         assertNull(empty.peekFirst());
     }
 
@@ -225,10 +255,9 @@ public class TSBDeQueueTest
      * Test of peekLast method, of class TSBDeQueue.
      */
     @org.junit.Test
-    public void testPeekLast() 
-    {
+    public void testPeekLast() {
         System.out.println("peekLast");
-        assertEquals((Integer)11, instance.peekLast());
+        assertEquals((Integer) 11, instance.peekLast());
         assertNull(empty.peekLast());
     }
 
@@ -236,47 +265,54 @@ public class TSBDeQueueTest
      * Test of removeFirstOccurrence method, of class TSBDeQueue.
      */
     @org.junit.Test
-    public void testRemoveFirstOccurrence() 
-    {
+    public void testRemoveFirstOccurrence() {
         System.out.println("removeFirstOccurrence");
         assertTrue(instance.removeFirstOccurrence(7));
         assertFalse(instance.removeFirstOccurrence(99));
-        assertFalse(empty.removeFirstOccurrence(10));       
+        assertFalse(empty.removeFirstOccurrence(10));
+        // Controla que se haya eliminado el primer 7 y no otro.
+        Iterator it = instance.iterator();
+        it.next();
+        it.next();
+        assertEquals((Integer) 6, it.next());
+
     }
 
     /**
      * Test of removeLastOccurrence method, of class TSBDeQueue.
      */
     @org.junit.Test
-    public void testRemoveLastOccurrence() 
-    {
+    public void testRemoveLastOccurrence() {
         System.out.println("removeLastOccurrence");
         assertTrue(instance.removeLastOccurrence(7));
         assertFalse(instance.removeLastOccurrence(99));
-        assertFalse(empty.removeLastOccurrence(10));       
+        assertFalse(empty.removeLastOccurrence(10));
+        // Controla que no se haya eliminado el primer 7
+        Iterator it = instance.iterator();
+        it.next();
+        it.next();
+        assertEquals((Integer) 7, it.next());
     }
 
     /**
      * Test of offer method, of class TSBDeQueue.
      */
-    @org.junit.Test (expected = NullPointerException.class)
-    public void testOffer() 
-    {
+    @org.junit.Test(expected = NullPointerException.class)
+    public void testOffer() {
         System.out.println("offer");
         assertTrue(instance.offer(101));
-        assertEquals((Integer)101, instance.getLast());
+        assertEquals((Integer) 101, instance.getLast());
         instance.offer(null);
     }
 
     /**
      * Test of remove method, of class TSBDeQueue.
      */
-    @org.junit.Test (expected = NoSuchElementException.class)
-    public void testRemove() 
-    {
+    @org.junit.Test(expected = NoSuchElementException.class)
+    public void testRemove() {
         System.out.println("remove");
         int ta = instance.size();
-        assertEquals((Integer)1, instance.remove());
+        assertEquals((Integer) 1, instance.remove());
         assertEquals(ta - 1, instance.size());
         int r1 = empty.remove();
     }
@@ -285,11 +321,10 @@ public class TSBDeQueueTest
      * Test of poll method, of class TSBDeQueue.
      */
     @org.junit.Test
-    public void testPoll() 
-    {
+    public void testPoll() {
         System.out.println("pollFirst");
         int ta = instance.size();
-        assertEquals((Integer)1, instance.poll());
+        assertEquals((Integer) 1, instance.poll());
         assertEquals(ta - 1, instance.size());
         assertNull(empty.poll());
     }
@@ -297,11 +332,10 @@ public class TSBDeQueueTest
     /**
      * Test of element method, of class TSBDeQueue.
      */
-    @org.junit.Test (expected = NoSuchElementException.class)
-    public void testElement() 
-    {
+    @org.junit.Test(expected = NoSuchElementException.class)
+    public void testElement() {
         System.out.println("element");
-        assertEquals((Integer)1, instance.element());
+        assertEquals((Integer) 1, instance.element());
         int r1 = empty.element();
     }
 
@@ -309,34 +343,31 @@ public class TSBDeQueueTest
      * Test of peek method, of class TSBDeQueue.
      */
     @org.junit.Test
-    public void testPeek() 
-    {
+    public void testPeek() {
         System.out.println("peek");
-        assertEquals((Integer)1, instance.peek());
+        assertEquals((Integer) 1, instance.peek());
         assertNull(empty.peek());
     }
 
     /**
      * Test of push method, of class TSBDeQueue.
      */
-    @org.junit.Test (expected = NullPointerException.class)
-    public void testPush() 
-    {
+    @org.junit.Test(expected = NullPointerException.class)
+    public void testPush() {
         System.out.println("push");
         instance.push(99);
-        assertEquals((Integer)99, instance.getFirst());
+        assertEquals((Integer) 99, instance.getFirst());
         instance.push(null);
     }
 
     /**
      * Test of pop method, of class TSBDeQueue.
      */
-    @org.junit.Test (expected = NoSuchElementException.class) 
-    public void testPop() 
-    {
+    @org.junit.Test(expected = NoSuchElementException.class)
+    public void testPop() {
         System.out.println("pop");
         int ta = instance.size();
-        assertEquals((Integer)1, instance.pop());
+        assertEquals((Integer) 1, instance.pop());
         assertEquals(ta - 1, instance.size());
         int r1 = empty.pop();
     }
@@ -344,54 +375,78 @@ public class TSBDeQueueTest
     /**
      * Test of descendingIterator method, of class TSBDeQueue.
      */
-    @org.junit.Test
-    public void testDescendingIterator() 
-    {
+    @org.junit.Test (expected = NoSuchElementException.class)
+    public void testDescendingIterator() {
         System.out.println("descendingIterator");
         int as = instance.size();
         Iterator r1 = instance.descendingIterator();
-        assertNotEquals(null, r1);        
+        assertNotEquals(null, r1);
         assertTrue(r1.hasNext());
         assertNotEquals(null, r1.next());
         r1.remove();
         assertEquals(as - 1, instance.size());
+        // Next luego de un remove no se debe saltear elementos.
+        assertEquals((Integer) 8, r1.next());
+        // Opcional. 2 invocaciones seguida de remove deben fallar.
+        try {
+            r1.remove();
+            r1.remove(); // Debe lanzar la excepcion aqui.
+            fail(); 
+        } catch (IllegalStateException e) {
+            // Ok
+        }
+        // Si se invoca a next cuando hasNext da falso debe fallar.
+        Iterator i2 = empty.iterator();
+        i2.next();
     }
-    
+
     /**
      * Test of clone method, of class TSBDeQueue.
      */
     @org.junit.Test
-    public void testClone() 
-    {
+    public void testClone() {
         System.out.println("clone");
-        try
-        {
-            TSBDeQueue<Integer> copy = (TSBDeQueue<Integer>)instance.clone();
+        try {
+            TSBDeQueue<Integer> copy = (TSBDeQueue<Integer>) instance.clone();
             assertTrue(instance.equals(copy));
-        }
-        catch(CloneNotSupportedException e)
-        {
+            // Test de clonado profundo
+            instance.addFirst(50);
+            assertEquals((Integer) 1, copy.getFirst());
+        } catch (CloneNotSupportedException e) {
+            // No deberia ocurrir.
+            fail();
         }
     }
-    
+
     /**
      * Test of equals method, of class TSBDeQueue.
      */
     @org.junit.Test
-    public void testEquals() 
-    {
+    public void testEquals() {
         System.out.println("equals");
         assertTrue(instance.equals(instance));
         assertFalse(instance.equals(empty));
+        // Prueba que si tienen igual contenido de equals = true;
+        assertTrue(instance.equals(instance2));
+        // sin importar la cantidad de modificaciones.        
+        for (int i = 0; i < 100; i++) {
+            instance.addLast(1);
+        }
+        for (int i = 0; i < 100; i++) {
+            instance.removeLast();
+        }
+
+        assertTrue(instance.equals(instance2));
     }
-    
+
     /**
      * Test of hashCode method, of class TSBDeQueue.
      */
     @org.junit.Test
-    public void testhashCode() 
-    {
+    public void testhashCode() {
         System.out.println("hashCode");
         assertNotEquals(empty.hashCode(), instance.hashCode());
-    } 
+        // Un hashcode siempre debe ser mayor a 0.
+        assertNotEquals(0, empty.hashCode());
+    }
 }
